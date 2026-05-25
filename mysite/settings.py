b5eba8b2e376523f158e36a527d68ff9931ef982
Kiel <kiel@example.com>
2026-05-25 11:54:12 +0800

    Fixed missing static CSS configurations and local permissions

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,7 +151,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/kiel/mysite/static/'
 
 # Redirect paths for authentication
 LOGIN_URL = 'login'
